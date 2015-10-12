8101d1650dd607db51b9542d286ee7f0a1a1cf19
balajinagaraju <balajinagaraju@outlook.com>
2015-10-12 15:31:58 +0530

    ignore-certifcate-errors
    changes for overcoming ignore-certifcate-errors issue

--- KanbanFlowAutomatedTest/src/main/java/com/suvi/pages/GenericPageObject.java
+++ KanbanFlowAutomatedTest/src/main/java/com/suvi/pages/GenericPageObject.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GenericPageObject {
 
@@ -11,11 +12,18 @@ public class GenericPageObject {
 
 	public GenericPageObject() {
 
-		driver = new ChromeDriver();
+		// The below option is added to overcome an certificate error.
+		// "You are using an unsupported command-line flag:
+		// --ignore-certifcate-errors. Stability and security will suffer."
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("test-type");
+
+		driver = new ChromeDriver(options);
 		System.out.println("ChromeDriver is instantiated");
 		System.out.println(driver);
 		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		this.driver.manage().window().maximize();
+		// this.driver.switchTo().window(this.driver.getWindowHandle());
 
 	}
 
